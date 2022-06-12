843e9e322f6e98e7494f403bd4a0f56b4f13e5c2
vadzim <vadzimk@hotmail.com>
2022-06-12 09:45:49 -0700

    add resources: routing table, internet gateway; add diagram

--- main.tf
+++ main.tf
@@ -9,21 +9,6 @@ terraform {
   }
 }
 
-variable "tenancy" {}
-variable "user" {}
-variable "fingerprint" {}
-variable "key_file" {}
-variable "region" {}
-variable "compartment_id" {}
-variable "env_prefix" {
-  default="development"
-}
-variable "vcn_cidr_block" {}
-variable "subnet_cidr_block" {}
-
-
-
-
 provider "oci" {
    tenancy_ocid = var.tenancy
    user_ocid = var.user
@@ -54,5 +39,41 @@ resource "oci_core_subnet" "subnet-1" {
 
     #Optional
     display_name = "${var.env_prefix}-subnet-1"
+    # route_table_id = oci_core_route_table.test_route_table.id
 
+}
+
+# internet gateway
+
+resource "oci_core_internet_gateway" "internet_gateway" {
+    #Required
+    compartment_id = var.compartment_id
+    vcn_id = oci_core_vcn.my-vcn.id
+
+    #Optional
+    # enabled = var.internet_gateway_enabled #TODO check the default value
+    display_name = "${var.env_prefix}-my-vcn-ig"
+
+}
+
+# routing table
+
+resource "oci_core_route_table" "my-vcn-rt" {
+    #Required
+    compartment_id = var.compartment_id
+    vcn_id = oci_core_vcn.my-vcn.id
+
+    #Optional
+   
+    display_name = "${var.env_prefix}-my-vcn-rt"
+   
+    route_rules {
+        #Required
+        network_entity_id = oci_core_internet_gateway.internet_gateway.id
+
+        #Optional
+        # description = "internet gateway"  #check if it provides the correct destination 
+        # destination = "0.0.0.0/0"
+        # destination_type = "CIDR_BLOCK"
+    }
 }
---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "tenancy" {}
+variable "user" {}
+variable "fingerprint" {}
+variable "key_file" {}
+variable "region" {}
+variable "compartment_id" {}
+variable "env_prefix" {
+  default="dev"
+}
+variable "vcn_cidr_block" {}
+variable "subnet_cidr_block" {}
