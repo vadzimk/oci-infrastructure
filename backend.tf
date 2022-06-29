593fcf9a19d704facc293688d79fca660745ed76
Vadzim <vadzimk@hotmail.com>
2022-06-28 21:50:21 -0700

    CI debug terraform plan step - Error: HTTP remote state endpoint requires auth

--- backend.tf
+++ backend.tf
@@ -3,16 +3,6 @@
 
 terraform {
   backend "http" {
-  }
-}
-
-variable "gitlab_username" {}
-variable "remote_state_address" {}
-
-data "terraform_remote_state" "gitlab" {
-  backend = "http"
-
-  config = {
     username = "gitlab-ci-token"
     password = "$CI_JOB_TOKEN"
     address = "$TF_ADDRESS"
@@ -23,3 +13,21 @@ data "terraform_remote_state" "gitlab" {
     retry_wait_min = "5"
   }
 }
+
+variable "gitlab_username" {}
+variable "remote_state_address" {}
+
+#data "terraform_remote_state" "gitlab" {
+#  backend = "http"
+#
+#  config = {
+#    username = "gitlab-ci-token"
+#    password = "$CI_JOB_TOKEN"
+#    address = "$TF_ADDRESS"
+#    lock_address = "$TF_ADDRESS/lock"
+#    unlock_address = "$TF_ADDRESS/lock"
+#    lock_method = "POST"
+#    unlock_method = "DELETE"
+#    retry_wait_min = "5"
+#  }
+#}
