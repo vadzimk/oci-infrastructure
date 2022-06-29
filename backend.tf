beea55ccb015291269172742ef7ab6cf9e1421d3
Vadzim <vadzimk@hotmail.com>
2022-06-28 19:12:43 -0700

    CI debug terraform plan step - add remote state configuration

--- backend.tf
+++ backend.tf
@@ -1,7 +1,20 @@
 # gitlab stores the terraform state
 # https://docs.gitlab.com/ee/user/infrastructure/iac/terraform_state.html
 
-terraform {
-  backend "http" {
+
+data "terraform_remote_state" "gitlab" {
+  backend = "http"
+
+  config = {
+    address = var.remote_state_address
+    username = var.gitlab_username
+    password = "$CI_JOB_TOKEN"
   }
+}
+
+variable "gitlab_username" {}
+variable "remote_state_address" {}
+
+provider "gitlab" {
+  token = "$CI_JOB_TOKEN"
 }
