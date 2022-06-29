6e1e1ce11bd12feccca319ac5b1075d77014b938
Vadzim <vadzimk@hotmail.com>
2022-06-29 00:27:50 -0700

    CI debug terraform plan step - Error: HTTP remote state endpoint requires auth

--- backend.tf
+++ backend.tf
@@ -1,27 +1,8 @@
 # gitlab stores the terraform state
 # https://docs.gitlab.com/ee/user/infrastructure/iac/terraform_state.html
 
-#terraform {
-#  backend "http" {
-#    username = "gitlab-ci-token"
-#    password = "$CI_JOB_TOKEN"
-#    address = "$TF_ADDRESS"
-#    lock_address = "$TF_ADDRESS/lock"
-#    unlock_address = "$TF_ADDRESS/lock"
-#    lock_method = "POST"
-#    unlock_method = "DELETE"
-#    retry_wait_min = "5"
-#  }
-#}
-
-
-variable "gitlab_token" {}
-variable "remote_state_address" {}
-
-data "terraform_remote_state" "gitlab" {
-  backend = "http"
-# terraform block does not allow variable values, only constants
-  config = {
+terraform {
+  backend "http" {
     username = "gitlab-ci-token"
     password = var.gitlab_token
     address = var.remote_state_address
@@ -32,3 +13,22 @@ data "terraform_remote_state" "gitlab" {
     retry_wait_min = "5"
   }
 }
+
+
+variable "gitlab_token" {}
+variable "remote_state_address" {}
+
+#data "terraform_remote_state" "gitlab" {
+#  backend = "http"
+## terraform block does not allow variable values, only constants
+#  config = {
+#    username = "gitlab-ci-token"
+#    password = var.gitlab_token
+#    address = var.remote_state_address
+#    lock_address = "${var.remote_state_address}/lock"
+#    unlock_address = "${var.remote_state_address}/lock"
+#    lock_method = "POST"
+#    unlock_method = "DELETE"
+#    retry_wait_min = "5"
+#  }
+#}
